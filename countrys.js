50b9be30133020b447db87b66bd0e7a69be58a78
xpati <st89nikon@gmail.com>
2016-12-25 10:54:26 +0000

    new map

---
+++ countrys.js
@@ -0,0 +1,37 @@
+  var OriginX=[3,18.5,31.6,-9.5];
+  var OriginY=[28,-12.5,4.85,6.5];
+  
+var country1 = [
+  {
+      destination: {latitude: 42.546245, longitude: 1.601554},
+      name: 'Andorra'
+  }];
+
+
+var country0 = [
+  {
+      destination: { latitude: 39.399872, longitude: -8.224454 },
+      name: 'Portugal'
+  }];
+  
+
+
+var country2 = [
+  {
+      destination: { latitude: 23.424076, longitude: 53.847818 },
+      name: 'United Arab Emirates'
+  }];
+  
+
+var country3 = [
+  {
+      destination: { latitude: 33.93911, longitude:67.709953 },
+      name: 'Afghanistan'
+  }];
+  
+  var country4 = [
+  {
+          destination: {  latitude: 17.060816, longitude: -61.796428 },
+      name: 'Antigua and Barbuda'
+  }];
+ 
